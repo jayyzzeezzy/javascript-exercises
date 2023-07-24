e864d7aaa75d7d97dc62b37b3279661eda0eb95e
James Zhou <jam9es@gmail.com>
2023-07-24 13:54:33 -0700

    Fix a bug on line 2

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
 const leapYears = function(year) {
-    return year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0);
+    if (year % 100 === 0 && year % 400 !== 0) return false;
+    
+    if (year % 4 == 0 || year % 400 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
+
+// the TOP solution writes this in one line of code
+// const leapYears = function(year) {
+//     return year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0);
+// };
 
 // Do not edit below this line
 module.exports = leapYears;
