bf2dbb1798771f607398169915e7a43c5ec1eca2
James Zhou <jam9es@gmail.com>
2023-08-24 21:40:52 -0700

    Add logic to function declaration

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    if (number === 0) return 0;
+    if (number == "1") return 1;
+    let theNumBeforePreviousNumb = 0;
+    let previousNumb = 1;
+    let currentNumb = 0;
+    for (let i = 2; i <= number; i++) {
+        currentNumb = previousNumb + theNumBeforePreviousNumb;
+        theNumBeforePreviousNumb = previousNumb;
+        previousNumb = currentNumb;
+    };
+    return currentNumb;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+// let theNumBeforePreviousNumb = 0;
+// let previousNumb = 1;
+// let currentNumb = 0;
+
+// currentNumb = previousNumb + theNumBeforePreviousNumb;
+// previousNumb = currentNumb;
+// theNumBeforePreviousNumb = previousNumb;
+// return currentNumb;
