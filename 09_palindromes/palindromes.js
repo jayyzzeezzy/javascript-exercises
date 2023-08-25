f64527787272203ce9556f0db70930f17258b247
James Zhou <jam9es@gmail.com>
2023-08-24 17:45:25 -0700

    Complete the palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return processedString == processedString.split("").reverse().join("");
+    // const backwardArray = [...string].reverse();
+    // const forwardArray = [...string];
+    // let result = false;
+    // for (let i = 0; i < forwardArray.length; i++) {
+    //     for (let j = 0; j < backwardArray.length; j++) {
+    //         if (forwardArray[i].charCodeAt(i))
+    //         if (forwardArray[i] === backwardArray[j]) {
+    //             result = true;
+    //         }
+    //         else {
+    //             result = false;
+    //         };
+    //     };
+    // };
+    // return result;
 };
 
 // Do not edit below this line
