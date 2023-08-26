732a761e4b19f2027047a85ed92dda23ac809b04
James Zhou <jam9es@gmail.com>
2023-08-25 21:31:41 -0700

    Edit function logic to make it more accurate

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,16 +1,41 @@
 const findTheOldest = function (array) {
     return array.reduce((oldestPerson, currentPerson) => {
-        const oldestAge = getAge(
-            oldestPerson.yearOfBirth, 
-            oldestPerson.yearOfDeath
-        );
-        const currentAge = getAge(
-            currentPerson.yearOfBirth, 
-            currentPerson.yearOfDeath
-        );
-        return oldestAge < currentAge ? currentPerson : oldestPerson;
+      const oldestAge = getAge(oldestPerson.yearOfBirth, oldestPerson.yearOfDeath);
+      const currentAge = getAge(
+        currentPerson.yearOfBirth,
+        currentPerson.yearOfDeath
+      );
+      return oldestAge < currentAge ? currentPerson : oldestPerson;
     });
+  };
+  
+const getAge = function (birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
 };
+  
+// const findTheOldest = function (array) {
+//     return array.reduce((oldestPerson, currentPerson) => {
+//         const oldestAge = getAge(
+//             oldestPerson.yearOfBirth, 
+//             oldestPerson.yearOfDeath
+//         );
+//         const currentAge = getAge(
+//             currentPerson.yearOfBirth, 
+//             currentPerson.yearOfDeath
+//         );
+//         return oldestAge < currentAge ? currentPerson : oldestPerson;
+//     });
+// };
+
+//     const getAge = function (birth, death) {
+//         if (!death) {
+//             death = new Date().getFullYear;
+//         }
+//         return death - birth;
+//     };
 
     // return array.reduce((obj, person) => {
     //     let personAge = person.yearOfDeath - person.yearOfBirth;
@@ -23,13 +48,6 @@ const findTheOldest = function (array) {
     //     } 
     //     return obj;
     // });
-
-const getAge = function (birth, death) {
-    if (!death) {
-        death = new Date().getFullYear;
-    };
-    return death - birth;
-};
 
 // Do not edit below this line
 module.exports = findTheOldest;
